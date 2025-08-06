8a9eea327ee25d1d70b1f0b1498c5d435120d895
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-08-06 14:50:19 -0500

    V-1.0.0

---
+++ assets/js/loadTemplates.js
@@ -0,0 +1,16 @@
+function loadTemplate(id, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(err => console.error(`Error cargando ${url}:`, err));
+}
+
+// Cargar plantillas
+loadTemplate("header", "components/header.html");
+loadTemplate("footer", "components/footer.html");
+loadTemplate("contact", "components/contact.html");
+loadTemplate("hero", "components/hero.html");
+loadTemplate("hero2", "components/hero.html");
+loadTemplate("products", "components/products.html");
