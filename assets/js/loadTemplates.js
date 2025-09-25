3ccfb0bf1bbcfd928d687d22941c6ad07acdab33
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-09-25 10:18:04 -0500

    deploy-3

--- assets/js/loadTemplates.js
+++ assets/js/loadTemplates.js
@@ -13,13 +13,13 @@ function loadTemplate(id, url, callback) {
 }
 
 // Cargar plantillas
-loadTemplate("navbar", "components/navbar.html");
-loadTemplate("footer", "components/footer.html");
-loadTemplate("contact", "components/contact.html");
-loadTemplate("hero2", "components/herov2.html");
-loadTemplate("products", "components/products.html");
-loadTemplate("services", "components/services.html");
-loadTemplate("off", "components/offCanvas.html", () => {
+loadTemplate("navbar", "./components/navbar.html");
+loadTemplate("footer", "./components/footer.html");
+loadTemplate("contact", "./components/contact.html");
+loadTemplate("hero2", "./components/herov2.html");
+loadTemplate("products", "./components/products.html");
+loadTemplate("services", "./components/services.html");
+loadTemplate("off", "./components/offCanvas.html", () => {
     mostrarProducts();
     mostrarTotal();
     countProducts();
